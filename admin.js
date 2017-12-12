a705e91fb77eaba5336c6592c1443a6c6ce5d9d1
keith <kfhough@gmail.com>
2017-12-12 08:18:45 -0500

    Initial commit

---
+++ admin.js
@@ -0,0 +1,98 @@
+"use strict";
+
+const LOCAL_STORAGE_KEY = "asu-courses";
+
+var getById = function (id) { return document.getElementById(id); };
+var courses = [];
+
+var addClass = function() {
+
+    var course = {};
+    var courseName = getById("course-name").value;
+    var courseNumber = getById("course-number").value.toUpperCase();
+    var courseDate = getById("course-date").value;
+    var courseLength = getById("course-length").value;
+    var courseDay = getById("course-day").value;
+    var courseTime = getById("course-time").value;
+    var courseDesc = getById("course-desc").value;
+
+    validateInput(courseName, courseNumber, courseDate, courseLength, courseDay, courseTime, courseDesc);
+
+    course = {
+        name: courseName,
+        number: courseNumber,
+        date: courseDate,
+        length: courseLength,
+        day: courseDay,
+        time: courseTime,
+        description: courseDesc
+    };
+
+    courses.push(course);
+
+    addLocalStorage(courses);
+
+    addCourseToTable(courses);
+
+};
+
+var addCourseToTable = function(courses) {
+    
+};
+
+var addLocalStorage = function() {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(courses));
+};
+
+var validateInput = function(courseName, courseNumber, courseDate, courseLength, courseDay, courseTime, courseDesc) {
+
+    if (courseName === "") {
+        getById("error-course-name").innerHTML = "Enter a Class Name";
+    }
+
+    if (courseNumber === "") {
+        getById("error-course-number").innerHTML = "Enter a Class Number";
+    }
+    else {
+
+        var courseNumberPattern = /^[A-Z]{3}-\d{3}$/;
+
+        if (courseNumberPattern.test(courseNumber) === false) {
+            getById("error-course-number").innerHTML = "Must be ABC-123 Format";
+        }
+    }
+
+    if (courseDate === "") {
+        getById("error-course-start").innerHTML = "Enter a Start Date";
+    }
+    else {
+
+        var courseDatePattern = /^\d{2}\/\d{2}\/\d{4}$/;
+
+        if (courseDatePattern.test(courseDate) === false) {
+            getById("error-course-start").innerHTML = "Must be XX/XX/XXXX Format";
+        }
+    }
+
+    var courseDateObject = new Date(courseDate);
+
+    if (courseDateObject.toString() === "Invalid Date") {
+        getById("error-course-start").innerHTML = "Enter a Valid Date";
+    }
+    else {
+        var today = new Date();
+        var dateDifference = courseDateObject.getTime() - today.getTime();
+
+        if (dateDifference < 0) {
+            getById("error-course-start").innerHTML = "Enter a Future Date";
+        }
+    }
+
+    if (courseDesc === "") {
+        getById("error-course-description").innerHTML = "Enter a Class Description";
+     }
+};
+
+window.onload = function() {
+    getById("add-class").onclick = addClass;
+}
---
+++ slideshow.js
@@ -0,0 +1,30 @@
+$(document).ready(function() {
+
+    // create an array of the slide images
+    var image, imageCounter = 0, imageCache = [];
+
+    $("#slides img").each(function() {
+        image = new Image();
+        image.src = $(this).attr("src");
+        image.title = $(this).attr("alt");
+        imageCache[imageCounter] = image;
+        imageCounter++;
+    });
+
+    // start slide show
+    imageCounter = 0;
+    var nextImage;
+    setInterval(
+        function () {
+            $("#slider-caption").fadeOut(1000);
+            $("#img-slide").fadeOut(1000,
+        function() {
+            imageCounter = (imageCounter + 1) % imageCache.length;
+            nextImage = imageCache[imageCounter];
+            $("#img-slide").attr("src", nextImage.src).fadeIn(1000);
+            $("#slider-caption").text(nextImage.title).fadeIn(1000);
+          }
+    );
+},
+7000);
+})
