1be4f8cbb0a06f096beeaf81609f3bff8162a70a
keith <kfhough@gmail.com>
2017-12-20 21:33:57 -0500

    More checking and updating local storage

--- admin.js
+++ admin.js
@@ -5,28 +5,6 @@ const LOCAL_STORAGE_KEY = "asu-courses";
 var getById = function (id) { return document.getElementById(id); };
 var courses = [];
 
-var getLocalStorage = function() {
-    var test = localStorage.getItem(LOCAL_STORAGE_KEY);
-    if (test !== null) {
-        courses = JSON.parse(test);
-
-        for (var i = 0; i < courses.length; i++) {
-            getById("table-body-courses").innerHTML +=
-                "<tr><td>" + courses[i].name +
-                "</td><td>" + courses[i].number +
-                "</td><td>" + courses[i].date +
-                "</td><td>" + courses[i].length +
-                "</td><td>" + courses[i].day +
-                "</td><td>" + courses[i].time +
-                "</td><td>" + courses[i].description + "</td></tr>";
-        }
-    }
-};
-
-var addCourseToTable = function(courseName, courseNumber, courseDate, courseLength, courseDay, courseTime, courseDesc) {
-        getById("table-body-courses").innerHTML += "<tr><td>" + courseName + "</td><td>" + courseNumber + "</td><td>" + courseDate + "</td><td>" + courseLength + "</td><td>" + courseDay + "</td><td>" + courseTime + "</td><td>" + courseDesc + "</td></tr>";
-};
-
 var addClass = function() {
     var course = {};
     var courseName = getById("course-name").value;
@@ -50,15 +28,37 @@ var addClass = function() {
             };
 
             courses.push(course);
-
             addLocalStorage();
-
             addCourseToTable(courseName, courseNumber, courseDate, courseLength, courseDay, courseTime, courseDesc);
-
             refreshForm();
-
     };
+};
 
+var addCourseToTable = function(courseName, courseNumber, courseDate, courseLength, courseDay, courseTime, courseDesc) {
+        getById("table-body-courses").innerHTML += "<tr><td>" + courseName + "</td><td>" + courseNumber + "</td><td>" + courseDate + "</td><td>" + courseLength + "</td><td>" + courseDay + "</td><td>" + courseTime + "</td><td>" + courseDesc + "</td></tr>";
+};
+
+var addLocalStorage = function() {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(courses));
+};
+
+var getLocalStorage = function() {
+    var storage = localStorage.getItem(LOCAL_STORAGE_KEY);
+    if (storage !== null) {
+        courses = JSON.parse(storage);
+
+        for (var i = 0; i < courses.length; i++) {
+            addCourseToTable(course[i].name, course[i].number, course[i].date, course[i].length, course[i].day, course[i].time, course[i].desc)
+        }
+    }
+};
+
+var refreshForm = function() {
+    getById("course-name").value = "";
+    getById("course-number").value = "";
+    getById("course-date").value = "";
+    getById("course-desc").value = "";
+    getById("course-name").focus();
 };
 
 var validateInput = function(courseName, courseNumber, courseDate, courseDesc) {
@@ -145,19 +145,6 @@ var validateInput = function(courseName, courseNumber, courseDate, courseDesc) {
     }
     return isValid;
 };
-
-var addLocalStorage = function() {
-    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(courses));
-};
-
-var refreshForm = function() {
-    getById("course-name").value = "";
-    getById("course-number").value = "";
-    getById("course-date").value = "";
-    getById("course-desc").value = "";
-    getById("course-name").focus();
-};
-
 
 window.onload = function() {
     getLocalStorage();
